75db7bfe6b4ac75284e6396aa38367bb97ef8aa0
Hugo Vila <git@hugovila.nom.es>
2015-07-30 17:33:53 +0200

    Add routes about, contact, friends, friends.about failures

--- js/app.js
+++ js/app.js
@@ -1,11 +1,60 @@
+var Ember, App;
+
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+App.friends = [
+    { id: 1, name: "John", about: "Funny" },
+    { id: 2, name: "Mary", about: "Smart" },
+    { id: 3, name: "Henry", about: "Kind" }
+];
+
+App.Router.map(function () {
+    // put your routes here
+    'use strict';
+    this.route('about'); /* AboutRoute, AboutController, AboutView, about */
+    this.route('contact');
+
+    this.resource('friends', function () {
+        this.resource('issues');
+        this.route('about', {path: "/:friends_id/about"});
+    });
+
+    this.resource('failures', function () {
+        this.route('details');
+    });
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+    model: function () {
+        'use strict';
+        return ['red', 'yellow', 'blue', 'orange'];
+    }
+});
+
+App.FriendsRoute = Ember.Route.extend({
+    model: function () {
+        'use strict';
+        return App.friends;
+    }
+});
+
+App.FriendsIndexRoute = Ember.Route.extend({
+    model: function () {
+        'use strict';
+        return ['Pedro', 'Mateo', 'Juan', 'Lucas', 'José'];
+    }
+});
+
+App.FriendsAboutRoute = Ember.Route.extend({
+    model: function (params) {
+        'use strict';
+        return App.friends.findBy('id', parseInt(params.friends_id, 10));
+    }
+});
+
+App.FailuresIndexRoute = Ember.Route.extend({
+    model: function () {
+        'use strict';
+        return ['Pedro', 'Santiago', 'Andrés', 'Juan', 'Felipe', 'Bartolomé', 'Tomás', 'Mateo', 'Simón', 'Matías'];
+    }
 });
